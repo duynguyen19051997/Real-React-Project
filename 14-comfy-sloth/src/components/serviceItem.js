997fdbb1d67d83a873c5c27a87492bdde1307d88
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2023-03-17 08:44:36 +0700

    service

--- 14-comfy-sloth/src/components/serviceItem.js
+++ 14-comfy-sloth/src/components/serviceItem.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { BsCompass } from "react-icons/bs";
+import { BsCompass, BsHammer, BsClipboardDataFill } from "react-icons/bs";
 import classes from "./serviceItem.module.css";
 
 export const ServiceItem = (props) => {
   return (
     <article className={classes["service_item_container"]}>
-      <BsCompass className={classes["service_icon"]} />
-      <h4>mission</h4>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates,
-        ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde
-        numquam nisi
-      </p>
+      {props.type === 1 && <BsCompass className={classes["service_icon"]} />}
+      {props.type === 2 && <BsHammer className={classes["service_icon"]} />}
+      {props.type === 3 && (
+        <BsClipboardDataFill className={classes["service_icon"]} />
+      )}
+
+      <h4>{props.name}</h4>
+      <p>{props.details}</p>
     </article>
   );
 };
--- 14-comfy-sloth/src/components/services.js
+++ 14-comfy-sloth/src/components/services.js
@@ -4,30 +4,24 @@ import { ServiceItem } from "./serviceItem";
 import classes from "./services.module.css";
 
 export const Services = (props) => {
+  const details =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dolorum debitis consectetur reprehenderit non aliquam voluptates dolore aut vero consequuntur.";
   return (
     <section className={classes["services_container"]}>
       <div className={classes["services_center"]}>
         <div className={classes["title"]}>
           <h2>custom furniture built only for you</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-            dolorum debitis consectetur reprehenderit non aliquam voluptates
-            dolore aut vero consequuntur.
-          </p>
+          <p>{details}</p>
         </div>
         <div className={classes["service_list"]}>
-          <ServiceItem />
-          <ServiceItem />
-          <ServiceItem />
+          <ServiceItem type={1} name="mission" details={details} />
+          <ServiceItem type={2} name="vision" details={details} />
+          <ServiceItem type={3} name="history" details={details} />
         </div>
         <div className={classes["contact_container"]}>
           <h2>Join our newsletter and get 20% off</h2>
           <div className={classes["info_contact"]}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-              sint unde quaerat ratione soluta veniam provident adipisci cumque
-              eveniet tempore?
-            </p>
+            <p>{details}</p>
             <div className={classes["contact"]}>
               <form className={classes["contact_form"]}>
                 <input className={classes["form_input"]} name="mail" />
