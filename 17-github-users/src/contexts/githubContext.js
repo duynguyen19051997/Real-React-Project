efca9d4d7564849815606937fe6181be57e8135c
Nguyen Vu Anh Duy <nguyenvuanhduy1905@gmail.com>
2024-03-06 21:44:18 +0700

    Update context

--- 17-github-users/src/contexts/githubContext.js
+++ 17-github-users/src/contexts/githubContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { followers } from "./data/followers";
 import { user as defaultUser } from "./data/user";
@@ -12,28 +12,13 @@ export const GithubProvider = ({ children }) => {
   const { user, loginWithRedirect, logout } = useAuth0();
   const [myUser, setMyUser] = useState(null);
   const [githubUser, setGithubUser] = useState(defaultUser);
-  const [githubFollowers] = useState(followers);
-  const [githubRepos] = useState(repos);
+  const [githubFollowers, setGithubFollowers] = useState(followers);
+  const [githubRepos, setGithubRepos] = useState(repos);
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [rateError, setRateError] = useState({ show: false, message: "" });
 
-  const searchGithubUser = async (userName) => {
-    setIsLoading(true);
-    await axios(`${rootUrl}/users/${userName}`)
-      .then(({ data }) => {
-        setGithubUser(data);
-        toggleError(false, "");
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        toggleError(true, "there is no user with that username");
-        setIsLoading(false);
-      });
-  };
-
-  const checkRequests = () => {
+  const checkRequests = useCallback(() => {
     axios(`${rootUrl}/rate_limit`)
       .then(({ data }) => {
         let remaining = data.rate.remaining;
@@ -44,7 +29,47 @@ export const GithubProvider = ({ children }) => {
         }
       })
       .catch((err) => console.log(err));
-  };
+  }, []);
+
+  const searchGithubUser = useCallback(
+    async (userName) => {
+      setIsLoading(true);
+      const response = await axios(`${rootUrl}/users/${userName}`).catch(
+        (err) => {
+          console.log(err);
+        }
+      );
+
+      if (response) {
+        setGithubUser(response.data);
+        const { login, followers_url } = response.data;
+
+        await Promise.allSettled([
+          axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+          axios(`${followers_url}?per_page=100`),
+        ])
+          .then((results) => {
+            const status = "fulfilled";
+            if (results[0].status === status) {
+              setGithubRepos(results[0].value.data);
+            }
+            if (results[1].status === status) {
+              setGithubFollowers(results[1].value.data);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        toggleError(false, "");
+      } else {
+        toggleError(true, "there is no user with that username");
+      }
+      checkRequests();
+      setIsLoading(false);
+    },
+    [checkRequests]
+  );
 
   const toggleError = (show, message) => {
     setRateError({ show, message });
@@ -52,11 +77,11 @@ export const GithubProvider = ({ children }) => {
 
   useEffect(() => {
     checkRequests();
-  }, []);
+  }, [checkRequests]);
 
   useEffect(() => {
-    setMyUser(user);
-  }, [user]);
+    searchGithubUser("duynguyen19051997");
+  }, [searchGithubUser]);
 
   return (
     <GithubContext.Provider
